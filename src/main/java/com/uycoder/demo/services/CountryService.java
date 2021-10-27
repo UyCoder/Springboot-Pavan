ef8295a04b5c6aebb5a3888f67123b390ff586b7
Ahmed Bughra <ahmedbughra@gmail.com>
2021-10-27 14:40:33 +0300

    First 4 video finished.

--- src/main/java/com/uycoder/demo/services/CountryService.java
+++ src/main/java/com/uycoder/demo/services/CountryService.java
@@ -1,6 +1,8 @@
 package com.uycoder.demo.services;
 
 import com.uycoder.demo.beans.Country;
+import com.uycoder.demo.controllers.AddResponse;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,21 +25,25 @@ public class CountryService {
         countryIdMap.put(3, ETCountry);
     }
 
-    // get all countries data as List from Hashmap
+    // 1. get all countries data as List from Hashmap
+    // http://localhost:8081/getcountries
+
     public List getAllCountries() {
         List countries = new ArrayList(countryIdMap.values());
         return countries;
     }
 
-    // get country by its ID
+    // 2. get country by its ID
+    // http://localhost:8081/getcountries/1
+
     public Country getCourtyById(int id) {
         Country country = countryIdMap.get(id);
         return country;
     }
 
 
-    // get country by its name and if Country name is the same as name we entered
-    // it will return all the country datas
+    // 3. get country by its name and if Country name is the same as name we entered, it will return all the country datas
+    // http://localhost:8081/countryname?name=india
 
     public Country getCountryByName(String countryName) {
         Country country = null;
@@ -46,5 +52,40 @@ public class CountryService {
                 country = countryIdMap.get(i);
         }
         return country;
+    }
+
+    // 4. add new country
+    // http://localhost:8081/addcountry
+    public Country addCountry(Country country) {
+        country.setId(getMaxId());
+        countryIdMap.put(country.getId(), country);
+        return country;
+    }
+
+    // Utility method to get max id
+    public static int getMaxId() {
+        int max = 0;
+        for (int id : countryIdMap.keySet())
+            if (max <= id)
+                max = id;
+        return max + 1;
+    }
+
+    // 5. update existing country
+    // http://localhost:8081/updatecountry
+    public Country updateCountry(Country country) {
+        if(country.getId()>0)
+            countryIdMap.put(country.getId(), country);
+        return country;
+    }
+
+    // 6. delete country data
+    // http://localhost:8081/deletecountry/4
+    public AddResponse deleteCountry(int id) {
+        countryIdMap.remove(id);
+        AddResponse res = new AddResponse();
+        res.setMsg("Country deleted.");
+        res.setId(id);
+        return res;
     }
 }
