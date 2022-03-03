fceb1406bf3dbb04d4b0e7a47636a1e8acdf4928
ahmed <50663127+UyCoder@users.noreply.github.com>
2022-03-04 00:18:59 +0300

    Update DemoApplicationTests.java

--- src/test/java/com/uycoder/demo/DemoApplicationTests.java
+++ src/test/java/com/uycoder/demo/DemoApplicationTests.java
@@ -4,8 +4,6 @@ package com.uycoder.demo;
  * Package:     com.uycoder.demo.beans
  * Created:     2021-11-15  22:31
  * Author:      Ahmed Bughra
- * Email:       AhmedBughra@gmail.com
- * LinkedIn:    https://www.linkedin.com/in/ahmed-bughra/
  * CreatedWith: IntelliJ IDEA
  */
 import org.junit.jupiter.api.Test;
